c619b7f0c270a9c0e2323091396310b6505bb737
Sergio Gamundi Barrientos <sergio.gamundi+aea@aireuropa.com>
2026-02-18 13:07:00 +0100

    feat(pokemon): added partial support for CRUD with mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/03-pokedex'),
+
     PokemonModule,
+
+    SharedModule,
   ],
 })
 export class AppModule {}
